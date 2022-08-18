7f92e9260662d4d85f7f962869e06b766abddfeb
Gravitysause <adriantarantino2006@gmail.com>
2022-08-17 23:27:34 -0400

    Initial Commit with basic functionality written

---
+++ main.ino
@@ -0,0 +1,76 @@
+struct Motor {
+    // The motor struct is a readable way for 
+    // me to interact with each individual motor.
+
+    int pin1;       // The pins needed to send signals between the Arduino 
+    int pin2;       // to the L293D Quadruple Half-H Drivers inputs.
+
+    void move_forward() {
+        digitalWrite(pin2, LOW);
+        delay(100);
+        digitalWrite(pin1, HIGH);
+        delay(100);
+    }
+
+    void move_backward() {
+        digitalWrite(pin1, LOW);
+        delay(100);
+        digitalWrite(pin2, HIGH);
+        delay(100);
+    }
+
+    void stop() {
+        digitalWrite(pin1, LOW);
+        digitalWrite(pin2, LOW);
+        delay(100);
+    }
+};
+
+struct Car {
+    // The car struct is a readable way for me
+    // to control all functionality of the car.
+
+    ////////////////
+    // Components //
+    ////////////////
+
+    Motor right_motor;
+    Motor left_motor;
+
+    ///////////////////
+    // Functionality //
+    ///////////////////
+
+    void move_forward() {
+        right_motor.move_forward();
+        left_motor.move_forward();
+    }
+
+    void move_backward() {
+        right_motor.move_backward();
+        left_motor.move_backward();
+    }
+
+    void turn_left() {
+        right_motor.move_forward();
+        left_motor.stop();
+    }
+
+    void turn_right() {
+        right_motor.stop();
+        left_motor.move_forward();
+    }
+
+    void stop() {
+        right_motor.stop();
+        left_motor.stop();
+    }
+};
+
+void setup() {} // Setup isn't needed for this program.
+void loop() {
+    Car car = {
+        {4, 5},
+        {6, 7},
+    };
+}
